a5a90b6fa6324a221f775c4cb54008690885991c
Owain Lewis <owain@owainlewis.com>
2016-10-28 22:50:53 +0100

    Update btree

--- Binary Tree/binary_tree.ml
+++ Binary Tree/binary_tree.ml
@@ -4,8 +4,13 @@ type 'a binary_tree =
   | Node of 'a binary_tree * 'a * 'a binary_tree
   | Leaf
 
-(* Insert an item into the tree *)
-let insert tree item =
+let empty = Leaf
+
+let is_empty = function
+  | Leaf -> true
+  | Node _ -> false
+
+let rec insert tree item =
   match tree with
     | Leaf -> Node(Leaf, item, Leaf)
     | Node(l, v, r) ->
@@ -25,3 +30,16 @@ let rec contains tree item =
        if item < v then
          contains l item
        else contains r item
+
+let rec min_tree = function
+  | Leaf -> None
+  | Node(l, v, r) -> if (is_empty l) then v else min_tree l
+
+(*
+ * tree_map (fun x -> x + 1) (Node(Leaf, 10, Leaf))
+ *)
+let rec tree_map f = function
+  | Leaf ->
+      Leaf
+  | Node(l, v, r) ->
+      Node(tree_map f l, f v, tree_map f r)
