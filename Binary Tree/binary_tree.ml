880cf788eb41e45de152fb262413476448fa918d
Owain Lewis <owain@owainlewis.com>
2016-11-01 19:29:35 +0000

    Adds more utils

--- Binary Tree/binary_tree.ml
+++ Binary Tree/binary_tree.ml
@@ -4,6 +4,20 @@ type 'a binary_tree =
 
 let empty = Leaf
 
+let terminal v = Node(Leaf, v, Leaf)
+
 let is_empty = function
   | Leaf -> true
   | Node _ -> false
+
+(* Get the size of a binary tree *)
+let rec size = function
+  | Leaf -> 0
+  | Node(l, v, r) -> size l + 1 + size r
+
+(* Check if a binary tree contains a value *)
+let rec contains tree value =
+  match tree with
+  | Leaf -> false
+  | Node(l, v, r) -> if v == value then true
+                                   else contains l value || contains r value
